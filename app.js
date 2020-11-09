b832432abac6531aa099327cb3f6d442f5710dd2
Jorge Betancur <jbetancur@elespectador.com>
2020-11-09 17:30:24 +0000

    Basic Authentication

--- app.js
+++ app.js
@@ -32,6 +32,42 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log('====>', req.headers);
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    const err = new Error('You are not authenticated');
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    console.log('>====>', req.headers);
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  const auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
+    .toString()
+    .split(':');
+
+  const username = auth[0];
+  const password = auth[1];
+
+  if (username === 'admin' && password == 'password') {
+    next();
+  } else {
+    const err = new Error('You are not authenticated');
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
